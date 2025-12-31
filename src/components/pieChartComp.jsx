7f2537263057610a99feea6464ebe9d25cec0317
Aman-Maurya-coder <lastcodelearner@gmail.com>
2025-12-31 14:31:19 +0530

    refactor pie chart component: clean up unused imports and apply CSS classes for styling

--- src/components/pieChartComp.jsx
+++ src/components/pieChartComp.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-// import { Pie } from "react-chartjs-2";
-// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-// import { PieChart } from '@mui/x-charts/PieChart';
 import { PieChart, Pie, Label } from "recharts";
-import { RechartsDevtools } from "@recharts/devtools";
 
-// ChartJS.register(ArcElement, Tooltip, Legend);
 const MyPie = ({data}) => (
     <Pie
         data={data}
@@ -14,7 +9,6 @@ const MyPie = ({data}) => (
         outerRadius="80%"
         innerRadius="60%"
         isAnimationActive={false}
-
     />
 );
 
@@ -22,27 +16,10 @@ function PieChartComp({ chartData }) {
     return (
         <div
             id="pie-chart-container"
-            style={{
-                display: "flex",
-                flexWrap: "wrap",
-                width: "100%",
-                minHeight: "100px",
-                // border: "1px solid #ccc",
-                // padding: "10px",
-                justifyContent: "space-around",
-                alignItems: "stretch",
-            }}
+            className="pie-chart-container"
         >
             <PieChart
                 responsive
-                style={{
-                    height: "calc(100% - 20px)",
-                    width: "30%",
-                    maxHeight: "35vh",
-                    flex: "1 1 100px",
-                    aspectRatio: 1,
-                    zIndex: 1,
-                }}
                 className="pie-chart"
             >
                 <MyPie 
